07218086b2dd1beeef5ab7c2fccfe3c11e630780
shiyangyang <shiyangyang@news.cn>
2015-06-04 16:56:32 +0800

    markdown add

--- ppt/js/ppt.js
+++ ppt/js/ppt.js
@@ -1,1 +1,98 @@
-!function(t){window.console||(window.console={log:new Function}),t(document).ready(function(){var n=t('<ol id="easy-nav"></ol>').appendTo(t("body"));t(".main-panel > li[id]").each(function(){var e=t(this);n.append(t('<li class="'+e.attr("id")+'" ><a href="javascript:void(0);">'+e.find("h2").html()+"</a></li>").click(function(){i(e)})),n.children(":first").addClass("current")}),t("section footer").each(function(){var n=t.trim(t(this).text());n||t(this).text(document.title+" - "+t("head > [name='author']").attr("content"))});var e=t(".main-panel > li[id]").addClass("clearfix").first();t(".main-panel").css({top:0-e.position().top});var i=function(n){t(".main-panel").is(":animated")||n instanceof t&&n.not(e).length&&(e=n.eq(0),t("#easy-nav > li").removeClass("current").filter("."+n.attr("id")).addClass("current"),t(".main-panel").animate({top:0-e.position().top},{duration:500}))},o=function(){return i(e.nextAll("li[id]")),!1},a=function(){return i(e.prevAll("li[id]")),!1},r=-1,c=function(t){r=t;var n=e.find("[data-step]").eq(r);n.length?n.trigger("step"):r=-1},s=function(){c(++r)},l=function(){c(--r)};t(document).keydown(function(t){var n=t||window.event,e=n.keyCode||n.which;switch(e){case 38:return a();case 32:case 40:return o();case 37:return l();case 39:return s()}}),t("[link-to]").click(function(){return i(t(t(this).attr("link-to"))),!1}),t("script[type='code']").each(function(){var n=t.trim(t(this).html()),e=t(this);e.replaceWith(t("<pre class='prettyprint code'></pre>").text(n))}),prettyPrint(),t(".section-main [data-step]").on("step",function(){t(this).toggleClass("active")}),t("body").on("mousewheel",function(t){t.deltaY>0?a():o(),t.preventDefault()});var d=t('<div id="tip-title"></div>').appendTo("body");t(document).on("mouseover","[title]",function(){var n=t(this),e=n.offset(),i=n.width(),o=n.height();d.html(this.title).fadeIn().css({left:e.left+i,top:e.top+o})}).on("mouseout","[title]",function(){d.hide()})})}(jQuery);
+(function ($) {
+    window.console || (window.console = {log : new Function()});
+
+    $(document).ready(function(){
+        /**
+         * 生成简单导航
+         */
+        var nav = $('<ol id="easy-nav"></ol>').appendTo($('body'));
+        
+        $(".main-panel > li[id]").each(function(){
+            var $this = $(this);
+            nav.append($('<li class="'+$this.attr('id')+'" ><a href="javascript:void(0);">'+$this.find('h2').html()+'</a></li>').click(function(){
+                scrollTo($this);
+            }));
+            nav.children(':first').addClass('current');
+        });
+        /**
+         * 在每一页PPT底部展示PPT名称和作者
+         */
+        $("section footer").each(function(i){
+            var text = $.trim($(this).text());
+            if(!text){
+                $(this).text( document.title + ' - ' + $("head > [name='author']").attr('content'));
+            }
+        });
+        /**
+         * 绑定翻页效果,绑定前先聚焦第一个section
+         */
+        var sectionLayer = $(".main-panel > li[id]").addClass('clearfix').first();
+        $(".main-panel").css({top : (0 - sectionLayer.position().top)});
+        var scrollTo = function(layer){
+            if($(".main-panel").is(':animated'))return;
+            if(layer instanceof $ && layer.not(sectionLayer).length){
+                sectionLayer = layer.eq(0);
+                $('#easy-nav > li').removeClass('current').filter("."+layer.attr('id')).addClass('current');
+                $(".main-panel").animate({top : (0 - sectionLayer.position().top)}, {duration: 500});
+            }
+        },goNext = function(){scrollTo(sectionLayer.nextAll('li[id]'));return false;},
+          goPrev = function(){scrollTo(sectionLayer.prevAll('li[id]'));return false;};
+        /**
+         * 绑定PPT每页内的下一步效果
+         */
+        var focusIndex = -1;
+        var focusTo = function(index){
+                focusIndex = index;
+                var nnn = sectionLayer.find('[data-step]').eq(focusIndex);
+                nnn.length ? nnn.trigger('step') : focusIndex = -1;
+            },
+            focusNext = function(){focusTo(++focusIndex);},
+            focusPrev = function(){focusTo(--focusIndex);};
+        $(document).keydown(function(event){
+            var e = event || window.event;
+            var k = e.keyCode || e.which;
+            switch(k){
+                case 38:  return goPrev();
+                case 32:
+                case 40:  return goNext();
+                case 37:  return focusPrev();
+                case 39:  return focusNext();
+                default:  break;
+            }
+        });
+
+        $("[link-to]").click(function(){scrollTo( $( $(this).attr('link-to') ) );return false;});
+        /**
+         * code可视化&格式化
+         */
+        $("script[type='code']").each(function(){
+            var replace = $.trim( $(this).html() ), $this = $(this);
+            $this.replaceWith( $("<pre class='prettyprint code'></pre>").text(replace) );
+        });
+        prettyPrint();  
+        
+        /**
+         * CSS3动画绑定
+         */
+        $(".section-main [data-step]").on('step',function(){$(this).toggleClass('active');});
+        $('body').on('mousewheel',function(e){
+            (e.deltaY > 0) ? goPrev() : goNext();
+            e.preventDefault();
+        });
+
+        var tip = $('<div id="tip-title"></div>').appendTo('body');
+        $(document).on('mouseover','[title]',function(e){
+            var _t  = $(this), 
+                off = _t.offset(), 
+                width = _t.width(), 
+                height = _t.height();
+
+            tip.html(this.title).fadeIn().css({
+                left: off.left+width,
+                top : off.top+height
+            })
+        }).on('mouseout','[title]',function(){
+            tip.hide();
+        });
+    });
+})(jQuery);
