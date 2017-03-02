3361a41eb6e9a922bd2ec9c2f92876f56bde8696
shy2850 <shy2850@163.com>
2017-03-03 00:59:27 +0800

    Signed-off-by: shy2850 <shy2850@163.com>

--- src/jquery/tabs.js
+++ src/jquery/tabs.js
@@ -1,78 +1,88 @@
 // $.fn.tabs = function(){};
 $.fn.extend({
-	tabs: function(cfg){
-		cfg = $.extend({
-			auto: false,
-			interval: 1200,
-			current: 0,
-			type: 'show',
+    tabs: function(cfg){
+        cfg = $.extend({
+            auto: false,
+            interval: 1200,
+            current: 0,
+            type: 'show',
 
-			handle: '.handle',
-			handle_current: '.current',
-			show: '.show',
+            handle: '.handle',
+            handle_current: '.current',
+            show: '.show',
 
-			handle_event: 'click',
-			pause_event: 'mouseover',
-			run_event: 'mouseout',
+            handle_event: 'click',
+            pause_event: 'mouseover',
+            run_event: 'mouseout',
 
-			btn_next: '.next',
-			btn_prev: '.prev'
-		}, cfg);
+            btn_next: '.next',
+            btn_prev: '.prev'
+        }, cfg);
 
-		return this.each(function(){
-			var holder = $(this);	// holder
-			var btns = holder.find(cfg.handle).children();
-			var show = holder.find(cfg.show)
-			var items = show.children();
+        return this.each(function(){
+            var holder = $(this);    // holder
+            var btns = holder.find(cfg.handle).children();
+            var show = holder.find(cfg.show)
+            var items = show.children();
+            var prev = holder.find(conf.prev);
+            var next = holder.find(conf.next);
 
-			var index = cfg.focus;
-			var width = items.width();
-			var height = items.height();
+            var index = cfg.focus;
+            var width = items.width();
+            var height = items.height();
 
-			function render (i) {
-				index = i;
-				btns.eq(i).addClass(cfg.handle_current).siblings().removeClass(cfg.handle_current);
-				switch(cfg.type){
-					case "slide":
-						show.animate({
-							marginLeft: -width * i
-						});
-						break;
-					case "show": 
-						items.eq(i).show().siblings().hide();
-						break;
-					case "fade": 
-						items.eq(i).fadeIn().siblings().fadeOut();
-						break;
-					case "animate":
-						show.animate({
-							marginTop: -height * i
-						});
-						break;
-				}
-			}
+            function render (i) {
+                index = i;
+                btns.eq(i).addClass(cfg.handle_current).siblings().removeClass(cfg.handle_current);
+                switch(cfg.type){
+                    case "slide":
+                        show.animate({
+                            marginLeft: -width * i
+                        });
+                        break;
+                    case "show": 
+                        items.eq(i).show().siblings().hide();
+                        break;
+                    case "fade": 
+                        items.eq(i).fadeIn().siblings().fadeOut();
+                        break;
+                    case "animate":
+                        show.animate({
+                            marginTop: -height * i
+                        });
+                        break;
+                }
+            }
 
-			if( cfg.auto ){
-				var if_hover = false;
-				holder.on(cfg.pause_event, function(){
-					if_hover = true;
-				}).on(cfg.run_event, function(){
-					if_hover = false;
-				});
+            if( cfg.auto ){
+                var if_hover = false;
+                holder.on(cfg.pause_event, function(){
+                    if_hover = true;
+                }).on(cfg.run_event, function(){
+                    if_hover = false;
+                });
 
-				setInterval(function(){
-					if( if_hover ){return;}
-					index = (index + 1) % items.length;
-					render(index);
-				}, cfg.interval);
-			}
+                setInterval(function(){
+                    if( if_hover ){return;}
+                    index = (index + 1) % items.length;
+                    render(index);
+                }, cfg.interval);
+            }
 
-			btns.on("click", function(e){
-				render($(this).index());
-				e.preventDefault();
-			});
+            btns.on("click", function(e){
+                render($(this).index());
+                e.preventDefault();
+            });
 
-			render(cfg.current);
-		});
-	}
+            prev.on('click', function(){
+                render(index - 1);
+            });
+
+            next.on('click', function(){
+                render(index - 1);
+            });
+
+            render(cfg.current);
+        });
+    }
 });
