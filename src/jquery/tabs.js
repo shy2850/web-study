51e1b42084ee5dcc500cbddff787be1cf56a409f
shy2850 <shy2850@163.com>
2017-03-03 01:09:28 +0800

    v

--- src/jquery/tabs.js
+++ src/jquery/tabs.js
@@ -24,8 +24,8 @@ $.fn.extend({
             var btns = holder.find(cfg.handle).children();
             var show = holder.find(cfg.show)
             var items = show.children();
-            var prev = holder.find(cfg.prev);
-            var next = holder.find(cfg.next);
+            var prev = holder.find(cfg.btn_prev);
+            var next = holder.find(cfg.btn_next);
 
             var index = cfg.focus;
             var width = items.width();
